651e33050f4f1bbecf94c2394583b90f45de9b1d
Diego Bermudez <bermudezjimenezjuandiego@gmail.com>
2025-04-15 00:56:48 -0500

    Pattern corrected, needed Router instead of Rep

--- faculty/internal/server/server.go
+++ faculty/internal/server/server.go
@@ -21,19 +21,22 @@ type responseRequest struct{
 type FacultyServer struct {
 	listenPort int
 	minPrograms int
+	clients [][]byte
 }
 
 func NewFacultyServer(listenPort, minPrograms int) *FacultyServer {
 	return &FacultyServer{
 		listenPort: listenPort,
 		minPrograms: minPrograms,
+		clients: [][]byte{},
 	}
 }
 
 // method to run the zeromq request reply server
 func (s *FacultyServer) Run() error {
 	//start zeromq request reply server
-	socket := zmq4.NewRep(context.Background())
+	//socket := zmq4.NewRep(context.Background())
+	socket := zmq4.NewRouter(context.Background())
 	defer socket.Close()
 	if err := socket.Listen(fmt.Sprintf("tcp://*:%d", s.listenPort)); err != nil {
 		return errors.New(fmt.Sprint("Unable to start server at port ", s.listenPort))
@@ -49,22 +52,28 @@ func (s *FacultyServer) Run() error {
 func (s *FacultyServer) listenProgramRequests(socket zmq4.Socket) error{
 	for i := 0; i < s.minPrograms; i++ {
 		message, err := socket.Recv()
+		clientId := message.Frames[0]
 		if err != nil{
 			return err
 		}
 		log.Print(message.String())
 
-		response := responseRequest{
-			Status: "estatus",
-			ClassroomsAsigned: 5,
-			LabsAsigned: 10,
-		}
-		jsonBytes, err := json.Marshal(response)
-		if err != nil{
-			return err
-		}
-		zmq4.New
-		socket.Send(zmq4.NewMsgString(string(jsonBytes)))
+		s.clients = append(s.clients, clientId)
+	}
+
+
+	response := responseRequest{
+		Status: "estatus",
+		ClassroomsAsigned: 5,
+		LabsAsigned: 10,
+	}
+
+	jsonBytes, err := json.Marshal(response)
+	if err != nil{
+		return err
+	}
+	for _,c := range s.clients{
+		socket.Send(zmq4.NewMsgFrom(c, jsonBytes))
 	}
 	return nil
 }
