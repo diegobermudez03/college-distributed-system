f10bcbf324dfacf56ba31b5f96e0fc11dc637554
Diego Bermudez <bermudezjimenezjuandiego@gmail.com>
2025-04-14 20:12:40 -0500

    Program developed, not yet tested, but code implemented with req reply zeromq and flags

---
+++ program/cmd/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/go-zeromq/zmq4"
+)
+
+const (
+	//flags for arguments
+	nameArg = "--name"
+	semesterArg = "--semester"
+	classroomsArg = "--classrooms"
+	labsArg = "--labs"
+	facultyServerArg = "--faculty-server"
+)
+
+//models for request response processing
+type requestStructure struct{
+	ProgramName string	`json:"program-name" validate:"required"`
+	Semester    string	`json:"semester" validate:"required"`
+	Classrooms  *int		`json:"classrooms" validate:"required"`	
+	Labs        *int		`json:"labs" validate:"required"`
+}
+
+type responseStructure struct{
+	Status string `json:"status"`
+	Message string `json:"message"`
+	ClassroomsAsigned int `json:"classrooms-assigned"`
+	LabsAsigned int `json:"labs-assigned"`
+}
+
+func main() {
+	//check number of arguments, should be (using labels so that the order dont matter):
+	// --name=<program-name> --semester=<semester> --classrooms=<num-classrooms> --labs=<num-labs> --faculty-server=<faculty-address>
+	//	executable --name=Ingenieria-Sistemas --semester=2025-10 --classrooms=4 --labs=10 --faculty-server=30.125.147.36:5000  
+	if len(os.Args) < 6{
+		log.Fatal("invalid number of arguments")
+	} 
+
+	request := requestStructure{}
+	var facultyServer string
+	for _, arg := range os.Args{
+		//ignore if atribute doesnt start with --
+		if !strings.Contains(arg, "--"){
+			continue
+		}
+		parts := strings.Split(arg, "=")
+		if len(parts) != 2{
+			continue
+		}
+		switch parts[0]{
+			case nameArg:
+				request.ProgramName = parts[1]
+			case semesterArg:
+				request.Semester = parts[1]
+			case classroomsArg:
+				if classrooms, err :=strconv.Atoi(parts[1]); err == nil{
+					request.Classrooms = &classrooms
+				}
+			case labsArg:
+				if labs, err := strconv.Atoi(parts[1]); err == nil{
+					request.Labs = &labs
+				}
+			case facultyServerArg:
+				facultyServer = parts[1]
+		}
+	}
+	
+	//check if all the request attributes where obtained from the flags
+	if request.ProgramName == "" || request.Semester == "" || request.Classrooms == nil || request.Labs == nil{
+		log.Fatal("invalid request")
+	}
+
+	//establish connection with the faculty
+	socket := zmq4.NewReq(context.Background(), zmq4.WithDialerRetry(time.Second))
+	defer socket.Close()
+	if err := socket.Dial(facultyServer); err != nil {
+		log.Fatal("Couldnt connect with faculty:" , err)
+	}
+
+	jsonMessage, err := json.Marshal(request)
+	if err != nil{
+		log.Fatal("Internal error processing request")
+	}
+	message := zmq4.NewMsgString(string(jsonMessage))
+	socket.Send(message)
+	//request sent to the faculty with JSON structure
+
+	//wait for response
+	response, err := socket.Recv()
+	if err != nil{
+		log.Fatal("Error with request ", err.Error())
+	}
+	//unmarshal response
+	responseJson := responseStructure{}
+	if err := json.Unmarshal([]byte(response.String()), &responseJson); err != nil{
+		log.Fatal("Invalid response received")
+	}
+
+	//print result 
+	log.Printf("Received %s, %d classrooms expected: %d received.  %d labs expected: %d received.",
+		 responseJson.Status, *request.Classrooms, responseJson.ClassroomsAsigned, 
+		 *request.Labs, responseJson.LabsAsigned,
+	)
+
+	//store result in a file
+	file, err := os.Create(fmt.Sprintf("%s_%s.txt", request.ProgramName, request.Semester))
+	if err != nil{
+		log.Print("Unable to write file", err.Error())
+	}
+	defer file.Close()
+	file.Write(response.Bytes())
+
+}
